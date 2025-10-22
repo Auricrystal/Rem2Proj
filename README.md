717ac0d1034d82997c1c0e56ae4ccd5b853bd56d
Auricrystal <71680981+Auricrystal@users.noreply.github.com>
2025-10-22 16:06:35 -0400

    Update to most recent build with fixes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Remnant2
  
+Now includes https://github.com/JsonAsAsset/JsonAsAsset, a plugin that lets you import certain things from FModel
--- Source/Remnant/Public/PrismStone.h
+++ Source/Remnant/Public/PrismStone.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "CoreMinimal.h"
+#include "ItemQuantity.h"
 #include "EquipmentMod.h"
 #include "InspectInfo.h"
 #include "EPrismSegmentCategory.h"
@@ -17,7 +18,6 @@ class UStatInfoSet;
 UCLASS(Blueprintable)
 class REMNANT_API APrismStone : public AEquipmentMod {
     GENERATED_BODY()
-public:
 protected:
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     UDataTable* PrismStoneTable;
@@ -100,8 +100,8 @@ protected:
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     float FeedFinalCurveExpGiven;
     
-    //UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
-    //TArray<FItemQuantity> ItemsToConsumeOnFlush;
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
+    TArray<FItemQuantity> ItemsToConsumeOnFlush;
     
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     UConditionList* StoneFeedConditions;
@@ -117,6 +117,9 @@ protected:
     
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     UConditionList* StoneRemoveSegmentsConditions;
+    
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
+    UConditionList* StoneRemoveMythicSegmentsConditions;
     
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     UConditionList* PlayerRemoveSegmentsConditions;
@@ -141,6 +144,9 @@ public:
     
     UFUNCTION(BlueprintCallable, Reliable, Server)
     void ServerGenerateStartSeed();
+    
+    UFUNCTION(BlueprintCallable, Reliable, Server)
+    void ServerFlushSegment(int32 SegmentIndex, int32 NumRefundLevels);
     
     UFUNCTION(BlueprintCallable, Reliable, Server)
     void ServerFlushPrismStone(bool RemoveSegments, bool RemovePendingExp, bool RemoveFeedData, bool RemoveStoredLevels);
@@ -173,6 +179,11 @@ public:
     UFUNCTION(BlueprintCallable, BlueprintPure)
     bool IsComboSegmentByNameID(FName NameID) const;
     
+protected:
+    UFUNCTION(BlueprintCallable)
+    void InvalidateCharacterStats();
+    
+public:
     UFUNCTION(BlueprintCallable, BlueprintPure)
     bool HasSegmentWithNameID(FName NameID);
     
@@ -282,6 +293,9 @@ public:
     
     UFUNCTION(BlueprintCallable)
     bool CanRemoveSegments();
+    
+    UFUNCTION(BlueprintCallable)
+    bool CanRemoveMythicSegments();
     
     UFUNCTION(BlueprintCallable)
     bool CanFlushStone();
